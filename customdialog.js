85372ce2100a2e5c0eb13f79482448fa5a391e19
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-02-27 18:30:43 -0800

    Alert Fixes

--- customdialog.js
+++ customdialog.js
@@ -1,9 +1,11 @@
 // Custom Alert
-const screenAlert = document.getElementById('alertButton');
-const dialogBox = document.getElementById('alertDialog');
-console.log("hello");
-screenAlert.addEventListener('click', () => {
-    dialogBox.showModal();
+//FOR ALERT BUTTON 
+const alertButton = document.getElementById('alertt');
+const alertDial = document.getElementById('alertDial')
+
+
+alertButton.addEventListener('click', () => {
+  alertDial.showModal();
 });
 
 //Custom Confirm
