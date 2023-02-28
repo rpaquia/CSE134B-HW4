7b9c9ada97ead66cef5cd2ce95b1510a62c9b4a0
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-02-27 18:47:27 -0800

    Custom Confirm

--- customdialog.js
+++ customdialog.js
@@ -1,13 +1,17 @@
 // Custom Alert
 const screenAlert = document.getElementById('alertButton');
 const dialogBox = document.getElementById('alertDialog');
-console.log("hello");
+
 screenAlert.addEventListener('click', () => {
     dialogBox.showModal();
 });
 
 //Custom Confirm
+const screenConfirm = document.getElementById('confirmDialog');
+const dialogConfBox = document.getElementById('btn2');
 
-
+screenConfirm.addEventListener('click', () => {
+    dialogConfBox.showModal();
+})
 //Custom Prompt
 
