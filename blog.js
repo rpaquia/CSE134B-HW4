030ce53f69cc028b229439392aaf4feafb70fce7
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-01 15:25:52 -0800

    Fixing

--- blog.js
+++ blog.js
@@ -1,4 +1,16 @@
+
+let items = JSON.parse(localStorage.getItem("todo-list")) || [];
+
+
 // Function that adds items to blog 
 function addItem() {
-    
+    // gets value that is put inside <input>
+
 }
+
+//------------Code for opening dialog box------------
+const screenAddBtn = document.getElementById('addBtn');
+const addDialogPopup = document.getElementById('addDiagBox');
+screenAddBtn.addEventListener('click', () => {
+    addDialogPopup.showModal();
+});
