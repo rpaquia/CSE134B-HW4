55fab3c626b3b732739f853151bbcd58dfdb4967
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-02 10:52:04 -0800

    CRUD addItem

--- blog.js
+++ blog.js
@@ -5,9 +5,31 @@ let items = JSON.parse(localStorage.getItem("todo-list")) || [];
 // Function that adds items to blog 
 function addItem() {
     // gets value that is put inside <input> with querySelector
-    let inputBox = document.querySelector()
-    
+    let inputTitleBox = document.querySelector('nameInput');
+    let inputSumBox = document.querySelector('sumInput');
+    let titleItem = inputBox.value;
+    let summaryItem = inputBox.value;
+
+    // If either input is empty return and alert user
+    if(titleItem === "") {
+        return alert("Please enter Title");
+    }
+    if(summaryItem === "") {
+        return alert("Please enter Summary");
+    }
+
+    // If inputs are valid, add them to array
+    items.push({
+        
+    })
+
+
+
 }
+
+
+
+
 
 //------------Code for opening dialog box------------
 const screenAddBtn = document.getElementById('addBtn');
